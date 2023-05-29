27578fb737aaeef4b4391a5129aa535f531f5f47
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-05-29 21:08:11 +0100

    feat: adicionando a configuracao do axios

---
+++ src/services/index.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import nookies from "nookies";
+
+export const api: AxiosInstance = axios.create({
+    baseURL: "http://localhost:1337/api/",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${nookies.get(null).token}`,
+    },
+});
